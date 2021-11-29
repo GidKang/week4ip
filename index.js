98ab80a8417a20bfa8c88adc5400d9cdfa2cd3ce
GidKang <gideonm77@outlook.com>
2021-11-29 05:47:53 +0300

    Made further changes to index.js in hopes of creating a cart page that actually works.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let carts = document.querySlectorAll('.add-cart');
+let carts = document.querySelectorAll('.add-cart');
 
 let products = [
     {
@@ -38,18 +38,48 @@ let products = [
         inCart: 0
     },
 
-]
+];
 
 for (let i=0; i < carts.length; i++){
     carts[i].addEventListener('click', () =>{
-      cartNumbers()
+      cartNumbers(products[i]);
     })
 }
 
-function cartNumbers(){
-    let productNumbers = localStorage.getItem('cartNumbers');
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartnumbers');
 
-    productNumbers = parseInt(productNumbers);
-    
-    localStorage.setItem('cartNumbers', 1);
+    if(productNumbers){
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
 }
+function cartNumbers(product){
+    
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers = parseInt(productNumbers);
+
+    if( productNumbers ){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productnumbers + 1;
+    }else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItems(product);
+}
+function setItems(product) {
+    let cartItems = localStorage.getItem('productInCart');
+    
+    product.inCart = 1;
+
+    cartItems = {
+        [product.tag]: product
+    }
+    
+    localStorage.setItem("prodcutsInCart",JSON.stringify(cartItems));
+
+}
+
+onLoadCartNumbers();
